55c70c55b138d732834182d4eb56fb03a24c40f1
denomparkour <denomparkour@gmail.com>
2020-09-27 10:10:38 +0000

    Parts: gtfo

--- device.mk
+++ device.mk
@@ -230,10 +230,6 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.2-service \
